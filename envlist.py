ace7db68e48329394aa2664050453b2f9bbcf5d9
HuiyuHere <t.huiyu@gmail.com>
2024-09-09 03:15:16 +0000

    added envlist.py

--- envlist.py
+++ envlist.py
@@ -40,21 +40,24 @@ lock = FileLock(lock_path, timeout=time_out_secs)
 
 with lock:
     if pmode == RESET_MODE:
-        # create a list (named clist) of nevns environments with the 
+        # create a list (named clist) of nenvs environments with the 
         # prefix envprefix
-        # add code here
+        clist = []
+        for i in range(int(nenvs)):
+            clist.append(f"{envprefix}{i}")
     else:
         # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
             # append item to end of list
-            # add code here
+            clist.append(env)
         else:    
             # get and remove env from clist
-            # add code here
+            env = clist.pop(0)
             # return env name
             print(env)
 
     # save hickle file
     hickle.dump(clist, file_path, mode="w")
+
