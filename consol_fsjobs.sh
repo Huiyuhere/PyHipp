521c03449a086f38009e1892ef08bd4356607bda
AWS ParallelCluster user <ec2-user@ip-10-0-0-24.ap-southeast-1.compute.internal>
2024-09-15 13:07:39 +0000

    added files 1509

--- consol_fsjobs.sh
+++ consol_fsjobs.sh
@@ -14,9 +14,10 @@ for i in "${temp1[@]}"; do
 done
 
 cmd1=${cmd1::-1}
-cmd1="${cmd1} /data/src/PyHipp/ec2snapshot.sh"
+cmd1="${cmd1} /data/src/PyHipp/fsall-slurm.sh"
 
 echo $cmd1
 eval $cmd1
 
-arn:aws:sns:ap-southeast-1:203918889266:awsnotify --message "ConsoleJobSumbitted"
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:203918889266:awsnotify --message "FSJobDone"
+
--- ec2snapshot.sh
+++ ec2snapshot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@xx.xx.xx.xx "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED -n MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@52.221.180.60 "source ~/.bash_profile; /home/ec2-user/.local/bin/pcluster update-compute-fleet --status STOP_REQUESTED -n MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
+
+arn:aws:sns:ap-southeast-1:203918889266:awsnotify --message "EC2SnapshotCompleted"
